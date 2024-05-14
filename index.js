3e4dbb43ed54f8d97f862f27e73f03f9f453bf4e
Lucas Rocha <lucasbezerrar@gmail.com>
2024-05-14 08:14:57 -0300

    Tentando resolver erro ao conectar ao banco de dados

--- index.js
+++ index.js
@@ -5,36 +5,23 @@ const server = express();
 
 server.use(express.json());
 
-
 server.get('/list', async (req, res) => {
-    const connection = await getDBConnection();
+  try {
+      const connection = await getDBConnection();
 
-    const results = await connection.query(
-      'SELECT * FROM teachers'
-    );
-    res.json({ teachers: results });
+      const results = await connection.query(
+          'SELECT * FROM teachers'
+      );
+
+      console.log(results); // Adicionando um console.log para ver os resultados
+
+      res.json({professores: results}); // Enviando apenas os resultados
+  } catch (error) {
+      console.error("Erro ao obter dados do banco de dados:", error);
+      res.status(500).json({ error: "Erro ao obter dados do banco de dados" });
+  }
 });
 
 server.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
-
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Erro ao conectar-se ao banco de dados:', err);
-//     return;
-//   }
-//   console.log('Conexão bem-sucedida ao banco de dados');
-// });
-
-// function query(sql, args) {
-//   return new Promise((resolve, reject) => {
-//     connection.query(sql, args, (err, results) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(results);
-//       }
-//     });
-//   });
-// }
