607eb103f25e9065d38afcc84779e27392cbf3cf
Lucas Rocha <lucasbezerrar@gmail.com>
2024-05-26 10:14:33 -0300

    Add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const { usersController } = require('./src/controllers/usersController');
 const { teacherController } = require('./src/controllers/teachersController');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post('/signup', usersController.createUser);
 app.get('/login', usersController.authUser);
